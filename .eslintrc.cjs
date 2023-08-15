9bda8c2f74ebfa69e418a7b2f0e268aac7f346ce
bluejoyq <bluejoy32@gmail.com>
2023-08-16 01:41:01 +0900

    feat: ts init

---
+++ .eslintrc.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+};
---
+++ dist/index.js
@@ -0,0 +1,6 @@
+var A = /** @class */ (function () {
+  function A() {
+    console.log('A');
+  }
+  return A;
+})();
